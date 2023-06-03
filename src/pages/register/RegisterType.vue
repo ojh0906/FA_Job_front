893b55443879d06549cae54d4b60a458bd36e2d6
kaefikals@gmail.com <kaefikals@gmail.com>
2023-06-03 22:22:20 +0900

    남원0603/회원가입구분

--- src/pages/register/RegisterType.vue
+++ src/pages/register/RegisterType.vue
@@ -1,6 +1,51 @@
 <template>
-    <div class="section01">
-        afdafaf
+    <div class="section">
+        <div class="join_heaer">
+            <h3 class="join_bg">회원가입</h3>
+            <ul class="join_step">
+                <li class="join_step_01_on">01.회원유형 선택</li>
+                <li class="join_step_02">02.약관동의</li>
+                <li class="join_step_03">03.정보입력</li>
+                <li class="join_step_04">04.가입완료</li>
+            </ul>
+        </div>
+        <div class="join_member">
+            <ul>
+                <!--일반 회원가입-->
+                <li class="join01">
+                    <a href="#">
+                        <i class="i_icon"></i>
+                        <p>
+                            <em class="bold">일반</em>
+                            "회원가입"
+                        </p>
+                        <span class="btn">가입하기</span>
+                    </a>
+                </li>
+                <!--외국인 회원가입-->
+                <li class="join02">
+                    <a href="#">
+                        <i class="i_icon"></i>
+                        <p>
+                            <em class="bold">외국인</em>
+                            "회원가입"
+                        </p>
+                        <span class="btn">가입하기</span>
+                    </a>
+                </li>
+                <!--기업 회원가입-->
+                <li class="join03">
+                    <a href="#">
+                        <i class="i_icon"></i>
+                        <p>
+                            <em class="bold">외국인</em>
+                            "회원가입"
+                        </p>
+                        <span class="btn">가입하기</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
