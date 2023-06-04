5cabd15a0bf9e89424c51d19f2285005c47ad194
kaefikals@gmail.com <kaefikals@gmail.com>
2023-06-04 14:54:06 +0900

    남원0604/약관동의

--- src/pages/register/RegisterType.vue
+++ src/pages/register/RegisterType.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="section">
+        <!--
         <div class="join_header">
             <h3 class="join_bg">회원가입</h3>
             <ul class="join_step">
@@ -11,7 +12,6 @@
         </div>
         <div class="join_member">
             <ul>
-                <!--일반 회원가입-->
                 <li class="join01">
                     <a href="#">
                         <i class="i_icon"></i>
@@ -21,7 +21,6 @@
                         <span class="btn">가입하기</span>
                     </a>
                 </li>
-                <!--외국인 회원가입-->
                 <li class="join02">
                     <a href="#">
                         <i class="i_icon"></i>
@@ -31,7 +30,6 @@
                         <span class="btn">가입하기</span>
                     </a>
                 </li>
-                <!--기업 회원가입-->
                 <li class="join03">
                     <a href="#">
                         <i class="i_icon"></i>
@@ -42,6 +40,42 @@
                     </a>
                 </li>
             </ul>
+        </div>
+    -->
+
+    <!--약관동의-->
+        <div class="join_header">
+            <h3 class="join_bg">회원가입</h3>
+            <ul class="join_step">
+                <li class="join_step_01">01.회원유형 선택</li>
+                <li class="join_step_02_on">02.약관동의</li>
+                <li class="join_step_03">03.정보입력</li>
+                <li class="join_step_04">04.가입완료</li>
+            </ul>
+        </div>
+        
+        <div class="agree">
+            <!-- 서비스이용약관 -->
+            <div class="agree_box">
+				<div class="title">
+				    <input type="checkbox"> <h3>서비스 이용약관(필수)</h3>
+				    <button type="button" class="btn_up">
+						<i class="i_icon"></i>
+					</button>
+				</div>
+				<div class="cont_body">
+				    <div class="board">
+				        <div class="agree">
+				            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+						</div>
+					</div>
+				</div>
+			</div>
         </div>
     </div>
 </template>
