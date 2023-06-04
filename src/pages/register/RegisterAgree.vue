f5185560c73d4b6635cd8afa72a90c9304f11ce6
kaefikals@gmail.com <kaefikals@gmail.com>
2023-06-04 20:53:33 +0900

    남원0604/약관동의

--- src/pages/register/RegisterAgree.vue
+++ src/pages/register/RegisterAgree.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="section">
-            <!--약관동의-->
-            <div class="join_header">
+        <!--약관동의-->
+        <div class="join_header">
             <h3 class="join_bg">회원가입</h3>
             <ul class="join_step">
                 <li class="join_step_01">01.회원유형 선택</li>
@@ -17,27 +17,88 @@
 				<div class="title">
 				    <ul>
                         <li class="check"></li>
-                        <li class="h3">서비스 이용약관(필수)</li>
-                        <li class="dropdown"><img src="/image/register/down.png" alt="드롭다운아이콘입니다."></li>
+                        <li class="h3">서비스 이용약관 (필수)</li>
+                        <li class="dropdown"><a href=""><img src="/image/register/down.png" alt="드롭다운아이콘입니다."></a></li>
                     </ul>
-				    <button type="button" class="btn_up">
-						<i class="i_icon"></i>
-					</button>
+                </div>
+                <!--드롭다운
+			    <div class="cont_body">
+                    <div class="board">
+                        <div class="agree">
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                        </div>
+                    </div>
+                </div>
+                -->
+			</div>
+            
+            <!-- 개인정보처리방침 -->
+            <div class="agree_box02">
+				<div class="title">
+				    <ul>
+                        <li class="check"></li>
+                        <li class="h3">개인정보처리방침 (필수)</li>
+                        <li class="dropdown"><a href=""><img src="/image/register/down.png" alt="드롭다운아이콘입니다."></a></li>
+                    </ul>
 				</div>
-				<div class="cont_body">
-				    <div class="board">
-				        <div class="agree">
-				            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                <!--드롭다운
+			    <div class="cont_body">
+                    <div class="board">
+                        <div class="agree">
                             이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
                             이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
                             이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
                             이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
                             이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-						</div>
-					</div>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                        </div>
+                    </div>
+                </div>
+                -->
+			</div>
+            
+            <!-- 이벤트 정보 수집 동의 -->
+            <div class="agree_box03">
+				<div class="title">
+				    <ul>
+                        <li class="check"></li>
+                        <li class="h3">이벤트 정보 수집 동의 (선택)</li>
+                        <li class="dropdown"><a href=""><img src="/image/register/down.png" alt="드롭다운아이콘입니다."></a></li>
+                    </ul>
+				</div>
+                <!--드롭다운
+			    <div class="cont_body">
+                    <div class="board">
+                        <div class="agree">
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                        </div>
+                    </div>
+                </div>
+                -->
+			</div>
+
+            <!-- 이벤트 정보 수집 동의 -->
+            <div class="agree_box04">
+				<div class="title">
+				    <ul>
+                        <li class="check"></li>
+                        <li class="h3">전체동의</li>
+                    </ul>
 				</div>
 			</div>
         </div>
+
+        <div class="next">다음</div>
     </div>
 </template>
 
