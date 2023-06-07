2660b71e0e742ca2867d4209c3f4bdfa66779a91
jiwon <jw.kim@itso.co.kr>
2023-06-07 23:50:51 +0900

    update 회원가입 약관 동의 수정

---
+++ src/components/register/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="join_header">
+        <h3 class="join_bg">회원가입</h3>
+        <div class="join_step">
+            <span :class="step == 1 ? 'on' : ''">01.회원유형 선택</span>
+            <span :class="step == 2 ? 'on' : ''">02.약관동의</span>
+            <span :class="step == 3 ? 'on' : ''">03.정보입력</span>
+            <span :class="step == 4 ? 'on' : ''">04.가입완료</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['step'],
+    data() {
+        return {
+        }
+    },
+    components: {
+    }
+    ,
+    methods: {
+    },
+    mounted() {
+    }
+}
+
+</script>
+
+
+
+
--- src/pages/register/RegisterAgree.vue
+++ src/pages/register/RegisterAgree.vue
@@ -1,109 +1,144 @@
 <template>
-    <div class="agree_section">
-        <!--회원가입 순서-->
-        <div class="join_header">
-            <h3 class="join_bg">회원가입</h3>
-            <ul class="join_step">
-                <li class="join_step_01">01.회원유형 선택</li>
-                <li class="join_step_02_on">02.약관동의</li>
-                <li class="join_step_03">03.정보입력</li>
-                <li class="join_step_04">04.가입완료</li>
-            </ul>
-        </div>
+    <div class="agree_section" @click="clickSelect">
+        <!-- 회원가입 헤더 -->
+        <RegisterHeader step="2" />
 
+        <!-- 약관동의 -->
         <div class="agree">
-            <!-- 서비스이용약관 -->
-            <div class="agree_box01">
-				<div class="title">
-				    <ul>
-                        <li class="check"></li>
-                        <li class="h3">서비스 이용약관 (필수)</li>
-                        <li class="dropdown"><a href=""><img src="/image/register/down.png" alt="드롭다운아이콘입니다."></a></li>
-                    </ul>
-                </div>
-                <!--드롭다운
-			    <div class="cont_body">
-                    <div class="board">
-                        <div class="agree">
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                        </div>
+            <aside class="agree-wrap">
+                <!-- 서비스이용약관 -->
+                <div class="agree_box agree_box01">
+                    <div class="title">
+                        <ul class="agree-select">
+                            <div class="agree-select-wrap" @click="onSelect(1)">
+                                <div class="check-text">
+                                    <input type="checkbox" required name="service" />
+                                    <li class="h3">서비스 이용약관 (필수)</li>
+                                </div>
+                                <li class="dropdown">
+                                    <img class="dropdown-img" src="/image/register/down.png" alt="드롭다운아이콘입니다.">
+                                </li>
+                            </div>
+                            <div class="select-box" :class="this.agree_select1 ? 'select-on ' : 'select-off'">
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            </div>
+                        </ul>
                     </div>
                 </div>
-                -->
-			</div>
-            
-            <!-- 개인정보처리방침 -->
-            <div class="agree_box02">
-				<div class="title">
-				    <ul>
-                        <li class="check"></li>
-                        <li class="h3">개인정보처리방침 (필수)</li>
-                        <li class="dropdown"><a href=""><img src="/image/register/down.png" alt="드롭다운아이콘입니다."></a></li>
-                    </ul>
-				</div>
-                <!--드롭다운
-			    <div class="cont_body">
-                    <div class="board">
-                        <div class="agree">
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                        </div>
-                    </div>
-                </div>
-                -->
-			</div>
-            
-            <!-- 이벤트 정보 수집 동의 -->
-            <div class="agree_box03">
-				<div class="title">
-				    <ul>
-                        <li class="check"></li>
-                        <li class="h3">이벤트 정보 수집 동의 (선택)</li>
-                        <li class="dropdown"><a href=""><img src="/image/register/down.png" alt="드롭다운아이콘입니다."></a></li>
-                    </ul>
-				</div>
-                <!--드롭다운
-			    <div class="cont_body">
-                    <div class="board">
-                        <div class="agree">
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                            이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
-                        </div>
-                    </div>
-                </div>
-                -->
-			</div>
 
-            <!-- 이벤트 정보 수집 동의 -->
-            <div class="agree_box04">
-				<div class="title">
-				    <ul>
-                        <li class="check"></li>
-                        <li class="h3">전체동의</li>
-                    </ul>
-				</div>
-			</div>
-            
+                <!-- 개인정보처리방침 -->
+                <div class="agree_box agree_box02">
+                    <div class="title">
+                        <ul class="agree-select">
+                            <div class="agree-select-wrap" @click="onSelect(2)">
+                                <div class="check-text">
+                                    <input type="checkbox" required name="service" />
+                                    <li class="h3">개인정보처리방침 (필수)</li>
+                                </div>
+                                <li class="dropdown">
+                                    <img class="dropdown-img" src="/image/register/down.png" alt="드롭다운아이콘입니다.">
+                                </li>
+                            </div>
+                            <div class="select-box" :class="this.agree_select2 ? 'select-on ' : 'select-off'">
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- 이벤트 정보 수집 동의 -->
+                <div class="agree_box agree_box03">
+                    <div class="title">
+                        <ul class="agree-select">
+                            <div class="agree-select-wrap" @click="onSelect(3)">
+                                <div class="check-text">
+                                    <input type="checkbox" name="service" />
+                                    <li class="h3">이벤트 정보 수집 동의 (선택)</li>
+                                </div>
+                                <li class="dropdown">
+                                    <img class="dropdown-img" src="/image/register/down.png" alt="드롭다운아이콘입니다.">
+                                </li>
+                            </div>
+                            <div class="select-box" :class="this.agree_select3 ? 'select-on ' : 'select-off'">
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                                이벤트 수집 동의 문구 내용이 들어가는 영역입니다.<br>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- 이벤트 정보 수집 동의 -->
+                <div class="agree_box agree_box04">
+                    <div class="title">
+                        <ul>
+                            <div class="check-text">
+                                <input type="checkbox" name="service" />
+                                <li class="h3">전체동의</li>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </aside>
+
             <div class="agree_next">
-                <div><a href="#">다음</a></div>
+                <p>다음</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import RegisterHeader from '/src/components/register/Header.vue';
+
+export default {
+    data() {
+        return {
+            agree_select1: false,
+            agree_select2: false,
+            agree_select3: false,
+        }
+    },
+    components: {
+        RegisterHeader
+    },
+    methods: {
+        onSelect(num) {
+            if (num == 1) {
+                this.agree_select1 = !this.agree_select1
+                this.agree_select2 = false;
+                this.agree_select3 = false;
+            } else if (num == 2) {
+                this.agree_select2 = !this.agree_select2
+                this.agree_select1 = false;
+                this.agree_select3 = false;
+            } else if (num == 3) {
+                this.agree_select3 = !this.agree_select3
+                this.agree_select1 = false;
+                this.agree_select2 = false;
+            }
+        },
+        clickSelect(event) {
+            if (!event.target.classList.contains('select-box') && !event.target.classList.contains('h3') && !event.target.classList.contains('dropdown-img')) {
+                this.agree_select1 = false;
+            }
+        },
+    },
+    mounted() {
+    }
+}
 
 </script>
