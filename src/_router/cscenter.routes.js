73cbf1deafa7806dc0f365f5f7ab719bd3ea253f
kaefikals@gmail.com <kaefikals@gmail.com>
2023-06-20 01:22:23 +0900

    남원0620

--- src/_router/cscenter.routes.js
+++ src/_router/cscenter.routes.js
@@ -1,4 +1,5 @@
 import NoticeList from "/src/pages/cscenter/NoticeList.vue";
+import NoticeDetail from "/src/pages/cscenter/NoticeDetail.vue";
 import AskList from "/src/pages/cscenter/AskList.vue";
 import AskEnd from "/src/pages/cscenter/AskEnd.vue";
 import Ask from "/src/pages/cscenter/Ask.vue";
@@ -9,6 +10,7 @@ export default [
         path: "/cscenter/",
         children: [
             { path: "notice-list", name: 'NoticeList', component: NoticeList, meta: { title: "고객센터 - 공지사항" } },
+            { path: "notice-detail", name: 'NoticeDetail', component: NoticeDetail, meta: { title: "고객센터 - 공지사항 상세" } },
             { path: "ask-list", name: 'AskList', component: AskList, meta: { title: "고객센터 - 문의하기" } },
             { path: "ask-end", name: 'AskEnd', component: AskEnd, meta: { title: "고객센터 - 문의하기 완료" } },
             { path: "ask", name: 'Ask', component: Ask, meta: { title: "고객센터 - 문의하기 상세" } },
---
+++ src/pages/cscenter/NoticeDetail.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="notice_detail">
+        <!--타이틀-->
+        <div class="ndtitle_header">
+            <h3 class="title_bg">고객센터</h3>
+        </div>
+
+        <div class="notice02_box">
+            <div class="ndbox_l">
+                <div class="menu">
+                    <a href="#">
+                        <div class="menu01">
+                        공지사항
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="menu02">
+                        문의하기
+                        <i class="i_icon"></i>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="menu03">
+                        FAQ
+                        <i class="i_icon"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="ndbox_r">
+                <!--타이틀-->
+                <div class="ndbox_r_header">
+                    <h3>공지사항</h3>
+                </div>
+
+                <table class="nd_table">
+                    <th class="t_title02 th_01" style="width: 688px;">제목이 들어가는 영역입니다.</th>
+                    <th class="t_title02 th_02" style="width: 120px;">2023.06.15</th>
+                    <th class="t_title02 th_03" style="width: 32px;"><i class="i_icon"></i></th>
+                </table>
+
+                <div class="ndcont_area">
+                    공지사항 내용이 들어가는 영역입니다.<br>
+                    공지사항 내용이 들어가는 영역입니다.<br>
+                    공지사항 내용이 들어가는 영역입니다.<br>
+
+                    <div class="file">
+                        <i class="i_icon"></i>
+                        <p>image0.png</p>
+                        <p>image1.png</p>                        
+                    </div>
+                </div>
+
+                <table class="nd_next_table">
+                    <th class="t_title03 th_01" style="width: 66px;"><i class="i_icon"></i><p>다음글</p></th>
+                    <th class="t_title03 th_02" style="width: 624px;">제목이 들어가는 영역입니다.</th>
+                    <th class="t_title03 th_03" style="width: 120px;">2023.06.16</th>
+                </table>
+                <table class="nd_before_table">
+                    <th class="t_title03 th_01" style="width: 66px;"><i class="i_icon"></i><p>이전글</p></th>
+                    <th class="t_title03 th_02" style="width: 624px;">제목이 들어가는 영역입니다.</th>
+                    <th class="t_title03 th_03" style="width: 120px;">2023.06.16</th>
+                </table>
+
+                
+
+                
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+</script>
