0a48a3a7b4ff599803d303115efa958fe6787a18
jamie <woqja0@gmail.com>
2023-06-22 20:22:55 +0900

    메인 블랙리스트

--- src/components/BlackSlider.vue
+++ src/components/BlackSlider.vue
@@ -1,34 +1,33 @@
 <template>
-    <section class="black-slider-wrap max">
-        <div class="black-main-title-wrap">
-            <p class="main-title">
-
-            </p>
-        </div>
-        <swiper :slidesPerView="3" :spaceBetween="40" :loop="true"
-            :navigation="{ nextEl: '.black-button-next', prevEl: '.black-button-prev' }" :modules="modules"
-            class="black-slider">
-            <swiper-slide v-for="v in 4">
-                <article class="black-box">
-                    <p class="black-title">박O민 ㅇㅇㅇㅇㅇ업체 / 사기내용작성 {{v}}</p>
-                    <div class="black-sub">
-                        <p class="black-sub-title">분류<span>프리랜서</span></p>
-                        <p class="black-sub-title">피해사례<span>대금미지급</span></p>
-                        <p class="black-sub-title">등록일<span>2023.06.12</span></p>
-                    </div>
-                </article>
-            </swiper-slide>
-        </swiper>
-        <!--  navigation -->
-        <aside class="black-slide-btn-wrap">
-            <div class="black-button-prev">
-                <img class="more-icon" src="/image/main/next.png" />
-            </div>
-            <div class="black-button-next">
-                <img class="more-icon" src="/image/main/next.png" />
-            </div>
-        </aside>
-    </section>
+  <section class="black-slider-wrap max">
+    <div class="black-main-title-wrap">
+      <p class="main-title"></p>
+    </div>
+<!--    <swiper class="black-slider" :navigation="{ nextEl: '.project-button-next', prevEl: '.project-button-prev' }" :loop="true" :modules="modules">-->
+      <swiper :slidesPerView="3" :spaceBetween="40" :loop="true"
+              :navigation="{ nextEl: '.black-button-next', prevEl: '.black-button-prev' }" :modules="modules"
+              class="black-slider">
+      <swiper-slide  v-for="item in this.blacklistList">
+        <article class="black-box">
+          <p class="black-title">{{ item.title }}</p>
+          <div class="black-sub">
+            <p class="black-sub-title">분류<span>{{ item.target }}</span></p>
+            <p class="black-sub-title">피해사례<span>{{ item.type }}</span></p>
+            <p class="black-sub-title">등록일<span>{{ formattedDate(item.reg_date) }}</span></p>
+          </div>
+        </article>
+      </swiper-slide>
+    </swiper>
+    <!--  navigation -->
+    <aside class="black-slide-btn-wrap">
+      <div class="black-button-prev">
+        <img class="more-icon" src="/image/main/next.png" />
+      </div>
+      <div class="black-button-next">
+        <img class="more-icon" src="/image/main/next.png" />
+      </div>
+    </aside>
+  </section>
 </template>
 
 <script>
@@ -42,24 +41,27 @@ import 'swiper/css/navigation';
 import { Pagination, Navigation } from 'swiper';
 
 export default {
-    data() {
-        return {
-        }
-    },
-    components: {
-        Swiper,
-        SwiperSlide,
-        ProjectBox
-    },
-    setup() {
-        return {
-            modules: [Pagination, Navigation],
-        };
-    },
-    methods: {
-    },
-    mounted() {
+  props:['blacklistList'],
+  data() {
+    return {
     }
+  },
+  components: {
+    Swiper,
+    SwiperSlide,
+    ProjectBox
+  },
+  setup() {
+    return {
+      modules: [Pagination, Navigation],
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    console.log(this.blacklistList)
+    console.log(this.blackListEndPage)
+  }
 }
 
 </script>
