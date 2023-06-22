e73e8ce6597395e93b83cf69ef1d6877823de5e8
오준호 <jh.oh@itso.co.kr>
2023-06-22 22:03:57 +0900

    0622

---
+++ src/pages/mypage/resume/none.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="mypage">
+    <LeftGnb/>
+    <div class="content-container">
+      <div class="resume-container">
+        <!--    여기에    -->
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import LeftGnb from "/src/components/mypage/LeftGnb.vue";
+
+export default {
+  components: {
+    LeftGnb
+  },
+}
+
+</script>
