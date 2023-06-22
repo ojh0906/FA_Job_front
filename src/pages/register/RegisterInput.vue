22ea52dd0fd2f393017ac42feb8336021133b0ab
오준호 <jh.oh@itso.co.kr>
2023-06-22 16:02:37 +0900

    0622

--- src/pages/register/RegisterInput.vue
+++ src/pages/register/RegisterInput.vue
@@ -135,7 +135,7 @@
                     <p class="input-title">{{ this.isCompany ? '사업장 주소' : '주소' }}</p>
                     <div class="input-wrap">
                         <div class="field_input">
-                            <input type="address" name="address" placeholder="주소 입력">
+                            <input type="address" id="roadAddress" name="address" placeholder="주소 입력">
                         </div>
                         <div class="check-btn btn btn3" @click="search()">
                             주소 찾기
@@ -234,10 +234,7 @@ export default {
         },
       search(){ //@click을 사용할 때 함수는 이렇게 작성해야 한다.
         new window.daum.Postcode({
-          oncomplete: (data) => { //function이 아니라 => 로 바꿔야한다.
-            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-            // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
-            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+          oncomplete: (data) => {
             var roadAddr = data.roadAddress; // 도로명 주소 변수
             var extraRoadAddr = ''; // 참고 항목 변수
             // 법정동명이 있을 경우 추가한다. (법정리는 제외)
@@ -257,29 +254,6 @@ export default {
             // document.getElementById('postcode').value = data.zonecode;
             document.getElementById("roadAddress").value = roadAddr;
             // document.getElementById("jibunAddress").value = data.jibunAddress;
-
-            // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
-            if(roadAddr !== ''){
-              document.getElementById("extraAddress").value = extraRoadAddr;
-            } else {
-              document.getElementById("extraAddress").value = '';
-            }
-
-            var guideTextBox = document.getElementById("guide");
-            // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
-            if(data.autoRoadAddress) {
-              var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-              guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-              guideTextBox.style.display = 'block';
-
-            } else if(data.autoJibunAddress) {
-              var expJibunAddr = data.autoJibunAddress;
-              guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-              guideTextBox.style.display = 'block';
-            } else {
-              guideTextBox.innerHTML = '';
-              guideTextBox.style.display = 'none';
-            }
           }
         }).open();
       }
