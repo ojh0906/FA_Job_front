8ebcdedae8ff0b772d0c61bc588a9903b78363d7
jiwon <jw.kim@itso.co.kr>
2023-06-28 19:41:21 +0900

    update faq

--- src/pages/cscenter/Faq.vue
+++ src/pages/cscenter/Faq.vue
@@ -1,50 +1,26 @@
 <template>
     <div div id="faq_section">
-        <!-- 회원가입 헤더 -->
         <div class="faqtitle_header">
             <h3 class="title_bg">고객센터</h3>
         </div>
 
         <div class="faq_box">
             <Menu click="3" />
-
             <div class="faq_r">
                 <div class="faq_r_header">
                     <h3>FAQ</h3>
                 </div>
-
                 <div class="accordion-container">
-                    <div class="faq-acco">
-                        <div class="accordion-label label01">
-                            <p>작성된 프로젝트 수정 방법</p>
-                            <i class="i_icon up"></i>
+                    <div class="faq-acco" v-for="(faq, idx) in faqlist_list" :key="idx">
+                        <div class="accordion-label label01" @click="this.openAccordion(idx)">
+                            <p>{{ faq.question }}</p>
+                            <i :class="this.accordions[idx] ? 'i_icon up' : 'i_icon down'"></i>
                         </div>
-                        <div class="accordion-cont">
+                        <div class="accordion-cont" ref="panel" :class="this.accordions[idx] ? 'open' : 'close'">
                             <div class="text-wrap">
                                 <strong>A.</strong>
-                                <h3>프로젝트 > 게시글 클릭 > 하단 수정</h3>
-                                <p>버튼을 눌러 수정해주세요.</p>
+                                <div v-html="faq.answer"></div>
                             </div>
-                        </div>
-
-                        <div class="accordion-label">
-                            <p>블랙리스트 해제 방법</p>
-                            <i class="i_icon down"></i>
-                        </div>
-
-                        <div class="accordion-label">
-                            <p>프로젝트에 문제가 생겼을시 대처 방법</p>
-                            <i class="i_icon down"></i>
-                        </div>
-
-                        <div class="accordion-label">
-                            <p>아이디 / 비밀번호 찾는 방법</p>
-                            <i class="i_icon down"></i>
-                        </div>
-
-                        <div class="accordion-label">
-                            <p>FA JOB 이용방법</p>
-                            <i class="i_icon down"></i>
                         </div>
                     </div>
                 </div>
@@ -73,9 +49,36 @@ export default {
 
     data() {
         return {
+            faqlist_list: [],
+            accordions: []
         }
     },
-
+    methods: {
+        getList() {
+            this.faqlist_list = [];
+            this.faqStore.list().then((resp) => {
+                if (resp.data.code == 200) {
+                    this.faqlist_list = resp.data.body;
+                    this.accordions = Array(this.faqlist_list.length).fill(false);
+                }
+            }).catch(err => {
+                console.log("err", err);
+            });
+        },
+        closeAllAccordion() {
+            this.accordions.forEach((_, idx) => {
+                this.accordions[idx] = false;
+            })
+        },
+        openAccordion(idx) {
+            this.closeAllAccordion();
+            this.accordions[idx] = true;
+        },
+    },
+    mounted() {
+        this.commonStore.getField();
+        this.getList();
+    }
 }
 </script>
 
