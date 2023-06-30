1805bbb6f3126a3933a882884eb2390876d31f35
jamie <woqja0@gmail.com>
2023-06-30 18:22:16 +0900

    프로젝트 조회

--- src/_stores/project.js
+++ src/_stores/project.js
@@ -8,7 +8,34 @@ export const useProjectStore = defineStore({
   state: () => ({
   }),
   actions: {
-
+    async list(option = { }, pages = { page: 1, page_block: 10 }) {
+      let params = {
+        ...option,
+        ...pages,
+      };
+      return await http.post(`${baseUrl}/list`, params);
+    },
+    async getById(id) {
+      return await http.get(`${baseUrl}/${id}`);
+    },
+    async save(params) {
+      return await http.post(`${baseUrl}/save`, params, { headers: { "Content-Type": "multipart/form-data" } });
+    },
+    async modify(id, params) {
+      return await http.put(`${baseUrl}/${id}`, params, { headers: { "Content-Type": "multipart/form-data" } });
+    },
+    async remove(id) {
+      return await http.delete(`${baseUrl}/${id}`);
+    },
+    async saveClick(id, params) {
+      return await http.post(`${baseUrl}/${id}/click/save`, params);
+    },
+    async saveLike(id, params) {
+      return await http.post(`${baseUrl}/${id}/like/save`, params);
+    },
+    async saveApply(id, params) {
+      return await http.post(`${baseUrl}/${id}/apply/save`, params);
+    },
   },
   persist: true,
 });
