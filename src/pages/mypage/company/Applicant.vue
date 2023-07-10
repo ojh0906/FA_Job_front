eb3d28958ec56f88156b47b78843ffc2f1419d08
jiwon <jw.kim@itso.co.kr>
2023-07-10 16:37:10 +0900

    update 지원자 목록 - 슬라이드 수정

--- src/pages/mypage/company/Applicant.vue
+++ src/pages/mypage/company/Applicant.vue
@@ -5,7 +5,8 @@
       <div class="mypage-info-container mypage-apply-container">
         <p class="area-title">{{ this.project.name }}</p>
         <p class="area-side">
-          지원자 <span class="num">{{ this.apply_list_total }}</span> 명 <span class="line">|</span> 이름 및 팀명을 클릭하면 지원자/지원팀의 이력서를 볼 수 있습니다.
+          지원자 <span class="num">{{ this.apply_list_total }}</span> 명 <span class="line">|</span> 이름 및 팀명을 클릭하면 지원자/지원팀의
+          이력서를 볼 수 있습니다.
         </p>
 
         <table class="list-table">
@@ -72,8 +73,8 @@
           <a v-if="this.apply_pages.start !== 1" @click="onChangePage(this.apply_pages.start - 1)">
             <img src="/image/community/back.png" alt="뒤로가기버튼입니다.">
           </a>
-          <a :class="this.apply_pages.page == page ? 'active' : 'pointer'"
-          v-for="page in this.apply_pages.pagesList" @click="onChangePage(page)">
+          <a :class="this.apply_pages.page == page ? 'active' : 'pointer'" v-for="page in this.apply_pages.pagesList"
+            @click="onChangePage(page)">
             {{ page }}
           </a>
           <a v-if="this.apply_pages.end !== this.apply_pages.end_page + 1"
@@ -84,7 +85,8 @@
 
         <div class="next-btn-wrap">
           <p class="people-cnt">
-            합격자/모집인원 <span class="num1">{{ this.apply_list.filter(i => i.pass).length }}</span><span class="num2"> / {{ this.project.people_cnt }}명</span>
+            합격자/모집인원 <span class="num1">{{ this.apply_list.filter(i => i.pass).length }}</span><span class="num2"> / {{
+              this.project.people_cnt }}명</span>
           </p>
           <div class="btn btn2" @click="">
             다운로드<img src="/image/mypage/download.png" />
@@ -122,48 +124,41 @@
 
   <!-- 지원자 이력서 팝업 -->
   <section id="popup" class="applicant-popup company-popup" v-if="this.applicantPopup" @click="this.clickSelect">
-    <div class="company-popup">
-      <swiper class="project-slider" :observer="true" :observe-parents="true"
-        :navigation="{ nextEl: '.popup-button-next', prevEl: '.popup-button-prev' }" :loop="true" :modules="modules">
-        <swiper-slide v-for="(apply, idx) in this.apply_list" :key="idx">
-          <!-- 개인 지원자 -->
-          <ApplicantPopup :idx="idx" @popup="onPopup" />
-        </swiper-slide>
-      </swiper>
-      <!--  navigation -->
-      <aside class="popup-slide-btn-wrap">
-        <div class="popup-button-prev">
-          <img class="more-icon" src="/image/mypage/pre.png" />
-        </div>
-        <div class="popup-button-next">
-          <img class="more-icon" src="/image/mypage/next.png" />
-        </div>
-      </aside>
-    </div>
+    <swiper :modules="modules" :navigation="{ nextEl: '.popup-button-next', prevEl: '.popup-button-prev' }"
+      :observer="true" :observe-parents="true" class="company-popup">
+      <swiper-slide v-for="(apply, idx) in this.apply_list" :key="idx">
+        <!-- 개인 지원자 -->
+        <ApplicantPopup :idx="idx" @popup="onPopup" />
+      </swiper-slide>
+    </swiper>
+    <!--  navigation -->
+    <aside class="popup-slide-btn-wrap">
+      <div class="popup-button-prev">
+        <img class="more-icon" src="/image/mypage/pre.png" />
+      </div>
+      <div class="popup-button-next">
+        <img class="more-icon" src="/image/mypage/next.png" />
+      </div>
+    </aside>
   </section>
 
   <!-- 팀정보 팝업 -->
-  <section id="popup" class="applicant-popup company-popup" v-if="this.applicantTeamPopup" @click="this.clickSelect">
-    <div class="company-popup">
-      <swiper class="project-slider" :observer="true" :observe-parents="true"
-        :navigation="{ nextEl: '.popup-button-next', prevEl: '.popup-button-prev' }" :loop="true" :modules="modules"
-        :initialSlide="3">
-        <swiper-slide v-for="(v, idx) in 3" :key="idx">
-          <!-- 팀 지원자 -->
-          <TeamPopup :applicantPopup="this.applicantPopup" @popup="onPopup" />
-        </swiper-slide>
-      </swiper>
-      <!--  navigation -->
-      <aside class="popup-slide-btn-wrap">
-        <div class="popup-button-prev">
-          <img class="more-icon" src="/image/mypage/pre.png" />
-        </div>
-        <div class="popup-button-next">
-          <img class="more-icon" src="/image/mypage/next.png" />
-        </div>
-      </aside>
-    </div>
-  </section>
+  <!-- <section id="popup" class="applicant-popup company-popup" v-if="this.applicantTeamPopup" @click="this.clickSelect">
+    <swiper :navigation="{ nextEl: '.popup-button-next', prevEl: '.popup-button-prev' }" :loop="true" :modules="modules"
+      :observer="true" :observe-parents="true" class="company-popup">
+      <swiper-slide v-for="(v, idx) in 3" :key="idx">
+        <TeamPopup :applicantPopup="this.applicantPopup" @popup="onPopup" />
+      </swiper-slide>
+    </swiper>
+    <aside class="popup-slide-btn-wrap">
+      <div class="popup-button-prev">
+        <img class="more-icon" src="/image/mypage/pre.png" />
+      </div>
+      <div class="popup-button-next">
+        <img class="more-icon" src="/image/mypage/next.png" />
+      </div>
+    </aside>
+  </section> -->
 
   <!-- 모집 완료 팝업 -->
   <section id="popup" class=" complete-popup" v-if="this.completePopup" @click="this.clickSelect">
@@ -264,6 +259,20 @@ export default {
         num_block: 5,
       },
       project: { name: '', people_cnt: 0 },
+
+      swiperOptions: {
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.popup-button-prev',
+          prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+          delay: 5000,
+        },
+      },
     }
   },
   methods: {
@@ -364,6 +373,6 @@ export default {
   mounted() {
     this.getProject();
     this.getApplyList();
-  }
+  },
 }
 </script>
