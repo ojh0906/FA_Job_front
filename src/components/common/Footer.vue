1c17d25863df68f66e4e7386ec15d9cdff6bc3a5
jiwon <jw.kim@itso.co.kr>
2023-07-17 11:55:43 +0900

    update 푸터 정보 수정

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -12,16 +12,16 @@
           <a class="link">이용약관</a>
         </div>
         <div class="line">
-          <span><strong>대표</strong>000</span>
-          <span><strong>주소</strong>서울특별시 중구 아무개동 아무개로 12-1</span>
+          <span><strong>대표</strong>박명호</span>
+          <span><strong>주소</strong>충남 아산시 탕정면 용두리 728, 유니콘101 522호</span>
         </div>
         <div class="line">
-          <span><strong>사업자등록번호</strong>000-00-0000000</span>
-          <span><strong>통신판매업신고</strong>제 0000-000000-0000호</span>
+          <span><strong>사업자등록번호</strong>121-35-63401</span>
+          <span><strong>통신판매업신고</strong>제 2021-충남천안-1745 호</span>
         </div>
         <div class="line">
-          <span><strong>EMAIL</strong>google@gmail.com</span>
-          <span><strong>TEL</strong>010-0000-0000</span>
+          <span><strong>EMAIL</strong>the-engineer@naver.com</span>
+          <span><strong>TEL</strong>010-7524-3560</span>
         </div>
       </div>
     </div>
